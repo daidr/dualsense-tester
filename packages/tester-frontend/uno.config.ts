b2563bee5252174b16328393e71cba59bd6ebd2f
daidr <daidr@daidr.me>
2023-10-17 22:31:49 +0800

    feat: colorful border support

--- packages/tester-frontend/uno.config.ts
+++ packages/tester-frontend/uno.config.ts
@@ -23,11 +23,12 @@ export default defineConfig({
   },
   shortcuts: {
     'dou-sc-capsule':
-      'px-2 lh-1.1em flex-shrink-0 text-primary/80 border-1.5 border-primary/30 rounded-full whitespace-nowrap',
+      'px-2 lh-1.1em flex-shrink-0 text-primary/80 dou-sc-colorborder rounded-full whitespace-nowrap',
     'dou-sc-btn':
-      'transition bg-transparent border-primary/20 border-1.5 text-primary px-4 py-1 rounded-full transform-gpu flex items-center gap-1 hover:(bg-primary text-white) active:(scale-90)',
+      'transition bg-transparent dou-sc-colorborder text-primary px-4 py-1 rounded-full transform-gpu flex items-center gap-1 hover:(bg-primary text-white) active:(scale-90)',
     'dou-sc-container': 'dou-sc-autoborder rounded-32px px-3 py-3',
     'dou-sc-autoborder': 'border-1 border-gray-3 dark-border-gray-6',
+    'dou-sc-colorborder': 'border-1.5 border-primary/20 dark-border-primary/50',
     'dou-sc-title': 'text-xl font-bold text-primary lh-1em',
     'dou-sc-link': 'text-primary hover:underline'
   }
