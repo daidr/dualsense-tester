7280d593d17ac2cdd63bc96bd23af660611fbe70
daidr <daidr@daidr.me>
2023-10-17 17:37:27 +0800

    feat: init info panel

---
+++ packages/tester-frontend/src/components/InfoPanel.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DualSenseModel from './DualSenseModel.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <DualSenseModel />
+    </div>
+</template>
+
+<style scoped>
+</style>
