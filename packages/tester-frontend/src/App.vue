3fa35c2a2f5d9238cc9020b91be7e0819657de52
daidr <daidr@daidr.me>
2023-10-17 23:04:39 +0800

    feat: add tips

--- packages/tester-frontend/src/App.vue
+++ packages/tester-frontend/src/App.vue
@@ -4,8 +4,9 @@ import MainFooter from './components/MainFooter.vue';
 import MainHeader from './components/MainHeader.vue';
 import { usePageStore } from './store/page';
 import ConnectPanel from './components/ConnectPanel.vue';
-import { useDualSenseStore } from './store/dualsense';
 import InfoPanel from './components/InfoPanel.vue';
+import { useDualSenseStore } from './store/dualsense';
+import ContentTips from './components/common/ContentTips.vue';
 
 const dualsenseStore = useDualSenseStore()
 const pageStore = usePageStore()
@@ -17,13 +18,19 @@ const { isWebHIDSupported } = storeToRefs(pageStore)
 
   <main>
     <template v-if="isWebHIDSupported">
-      <div class="grid grid-cols-1 lg:grid-cols-[400px_1fr] gap-2 min-h-[var(--min-height)]">
-        <ConnectPanel />
-        <div class="dou-sc-container">
-          <div v-if="!dualsenseStore.isConnected" class="h-full flex items-center justify-center text-lg text-primary/70">
-            Please connect your DualSense first
+      <div class="grid grid-cols-1 lg:grid-cols-[400px_1fr] gap-3 min-h-[var(--min-height)]">
+        <div class="flex flex-col gap-3 items-start">
+          <ConnectPanel />
+
+          <!-- TODO: Output report -->
+          <div v-if="dualsenseStore" class="dou-sc-container w-full">
+            <ContentTips icon="i-icon-park-twotone-code-laptop" :title="$t('output_panel.in_progress')">
+              {{ $t('output_panel.tips') }}
+            </ContentTips>
           </div>
-          <InfoPanel v-else />
+        </div>
+        <div class="dou-sc-container">
+          <InfoPanel />
         </div>
       </div>
     </template>
