bac36f4b8abe8624a9c018f0d46021ab8db80bf2
daidr <daidr@daidr.me>
2023-10-17 23:05:59 +0800

    feat: add language switch support

---
+++ packages/tester-frontend/src/components/LangSwitcher.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import SelectBox from './common/SelectBox.vue';
+import { getAvailableLanguages } from '@/utils/lang.util';
+
+const i18n = useI18n();
+
+const availableLocales = getAvailableLanguages();
+
+console.log(availableLocales)
+</script>
+
+<template>
+    <SelectBox v-model="i18n.locale.value" :options="availableLocales.map((locale) => ({
+        value: locale,
+        label: $t(`locales.${locale}`),
+    }))" />
+</template>
+
+<style scoped lang="scss"></style>
