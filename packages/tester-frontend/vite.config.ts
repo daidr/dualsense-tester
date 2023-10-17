eeb47eadb162b6b148c4781b49755658597ef2b7
daidr <daidr@daidr.me>
2023-10-17 21:56:57 +0800

    chore: defineModel support

--- packages/tester-frontend/vite.config.ts
+++ packages/tester-frontend/vite.config.ts
@@ -9,10 +9,15 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      script: {
+        defineModel: true
+      }
+    }),
     UnoCSS(),
     VueI18nPlugin({
-      include: [path.resolve(__dirname, './src/locales/**.locale.json')]
+      include: [path.resolve(__dirname, './src/locales/**.json')],
+      strictMessage: false
     })
   ],
   resolve: {
