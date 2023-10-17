3e03143eb103c329b5fb99c920aa2c4dff544cf0
daidr <daidr@daidr.me>
2023-10-17 17:36:46 +0800

    feat: add content tips

---
+++ packages/tester-frontend/src/components/common/ContentTips.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+    icon?: string
+    title?: string
+}>()
+</script>
+
+<template>
+    <div class="dou-sc-autoborder p-4 rounded-24px space-y-2">
+        <div class="flex text-primary font-bold items-center space-x-1 text-lg lh-1em">
+            <div :class="icon ?? 'i-icon-park-twotone-grinning-face-with-open-mouth'"></div>
+            <div>{{ title ?? 'Tips' }}</div>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
