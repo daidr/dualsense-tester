74cd15351ed55ea9d4c5027150fabe4aa45a4f9b
daidr <daidr@daidr.me>
2023-10-18 21:35:51 +0800

    feat: pwa support

--- packages/tester-frontend/vite.config.ts
+++ packages/tester-frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,42 @@ export default defineConfig({
     VueI18nPlugin({
       include: [path.resolve(__dirname, './src/locales/**.json')],
       strictMessage: false
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        '/pwa/android-chrome-192x192.png',
+        '/pwa/android-chrome-512x512.png',
+        '/pwa/apple-touch-icon.png',
+        '/pwa/favicon-16x16.png',
+        '/pwa/favicon-32x32.png',
+        '/pwa/mstile-70x70.png',
+        '/pwa/mstile-144x144.png',
+        '/pwa/mstile-150x150.png',
+        '/pwa/mstile-310x150.png',
+        '/pwa/mstile-310x310.png',
+        '/pwa/safari-pinned-tab.svg',
+      ],
+      manifest: {
+        start_url: 'https://ds.daidr.me',
+        display: 'standalone',
+        name: 'DualSense Tester',
+        short_name: 'DualSense Tester',
+        icons: [
+          {
+            src: '/pwa/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff'
+      }
     })
   ],
   resolve: {
