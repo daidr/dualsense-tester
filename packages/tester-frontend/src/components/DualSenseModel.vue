d1c4c29cd4bbad278bc199fa667673e7c4fd8f68
daidr <daidr@daidr.me>
2024-12-15 21:02:52 +0800

    feat: stick cross line support

--- packages/tester-frontend/src/components/DualSenseModel.vue
+++ packages/tester-frontend/src/components/DualSenseModel.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useDualSenseStore } from '@/store/dualsense';
 import { storeToRefs } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 defineProps<{
     showValue: boolean
@@ -30,11 +30,25 @@ const getTouchPointY = (y: number) => {
     return y * TOUCHPAD_YF + TOUCHPAD_TOP
 }
 
-const STICK_REAL_REDIUS = 40
+const STICK_REAL_REDIUS = 86
 
 const getStickPoint = (x: number) => {
     return STICK_REAL_REDIUS * x
 }
+
+const rightStickPosition = computed(() => {
+    return {
+        x: getStickPoint(state.value.axes.rightStickX),
+        y: getStickPoint(state.value.axes.rightStickY)
+    }
+})
+
+const leftStickPosition = computed(() => {
+    return {
+        x: getStickPoint(state.value.axes.leftStickX),
+        y: getStickPoint(state.value.axes.leftStickY)
+    }
+})
 </script>
 
 <template>
@@ -56,7 +70,7 @@ const getStickPoint = (x: number) => {
                 }" />
             <text v-if="showValue" x="150" y="30" font-size="25" class="ds-text" text-anchor="end">{{
                 state.axes.l2.toPrecision(8)
-                }}</text>
+            }}</text>
         </g>
         <g id="dpad-up">
             <path
@@ -139,25 +153,43 @@ const getStickPoint = (x: number) => {
         </g>
         <g id="r3group">
             <circle id="r3-border" cx="763.456" cy="528.548" r="87.347" class="ds-stroke-normal" />
-            <circle id="r3" cx="763.456" cy="528.548" r="57.193" class="ds-stroke-normal ds-stick"
-                :class="{ 'ds-active': state.buttons.r3 }" :style="{
-                    transform: `translate(${getStickPoint(state.axes.rightStickX)}px, ${getStickPoint(state.axes.rightStickY)}px)`
-                }" />
+            <g>
+
+                <circle id="r3" cx="763.456" cy="528.548" r="57.193" class="ds-stroke-normal ds-stick"
+                    :class="{ 'ds-active': state.buttons.r3 }" :style="{
+                        transform: `translate(${rightStickPosition.x}px, ${rightStickPosition.y}px)`
+                    }" />
+
+            </g>
+
             <text v-if="showValue" x="850" y="700" font-size="25" class="ds-text" text-anchor="end">{{
                 state.axes.rightStickX.toPrecision(9) }} X</text>
             <text v-if="showValue" x="850" y="730" font-size="25" class="ds-text" text-anchor="end">{{
                 state.axes.rightStickY.toPrecision(9) }} Y</text>
+            <!-- cross line -->
+            <line v-if="showValue" x2="763.456" y2="441.355" x1="763.456" y1="615.741" class="ds-stroke-dashed" />
+            <line v-if="showValue" x2="676.263" y2="528.548" x1="850.649" y1="528.548" class="ds-stroke-dashed" />
+            <circle v-if="showValue" cx="763.456" cy="528.548" r="2" class="ds-fill-red" :style="{
+                transform: `translate(${rightStickPosition.x}px, ${rightStickPosition.y}px)`
+            }" />
         </g>
         <g id="l3group">
             <circle id="l3-border" cx="351.764" cy="528.548" r="87.347" class="ds-stroke-normal" />
             <circle id="l3" cx="351.764" cy="528.548" r="57.193" class="ds-stroke-normal ds-stick"
                 :class="{ 'ds-active': state.buttons.l3 }" :style="{
-                    transform: `translate(${getStickPoint(state.axes.leftStickX)}px, ${getStickPoint(state.axes.leftStickY)}px)`
+                    transform: `translate(${leftStickPosition.x}px, ${leftStickPosition.y}px)`
                 }" />
             <text v-if="showValue" x="270" y="700" font-size="25" class="ds-text">X {{
                 state.axes.leftStickX.toPrecision(9) }}</text>
             <text v-if="showValue" x="270" y="730" font-size="25" class="ds-text">Y {{
                 state.axes.leftStickY.toPrecision(9) }}</text>
+            <!-- cross line -->
+            <line v-if="showValue" x2="351.764" y2="441.355" x1="351.764" y1="615.741" class="ds-stroke-dashed" />
+            <line v-if="showValue" x2="264.571" y2="528.548" x1="438.957" y1="528.548" class="ds-stroke-dashed" />
+            <!-- central point -->
+            <circle v-if="showValue" cx="351.764" cy="528.548" r="2" class="ds-fill-red" :style="{
+                transform: `translate(${leftStickPosition.x}px, ${leftStickPosition.y}px)`
+            }" />
         </g>
         <path id="ps"
             d="M525.1,538.049c-6.333,-1.817 -7.382,-5.56 -4.509,-7.734c2.071,-1.361 4.45,-2.545 6.969,-3.436l0.232,-0.072l18.72,-6.713l-0,7.71l-13.419,4.906c-2.351,0.905 -2.751,2.108 -0.798,2.754c0.974,0.248 2.089,0.392 3.239,0.392c1.641,-0 3.212,-0.291 4.666,-0.822l-0.094,0.029l6.462,-2.353l-0,6.915c-0.4,0.099 -0.854,0.147 -1.299,0.243c-2.068,0.363 -4.448,0.571 -6.875,0.571c-4.744,0 -9.3,-0.795 -13.547,-2.257l0.293,0.088l-0.04,-0.221Zm39.449,0.789l20.99,-7.584c2.383,-0.859 2.753,-2.084 0.819,-2.727c-0.968,-0.237 -2.078,-0.373 -3.222,-0.373c-1.66,-0 -3.255,0.288 -4.733,0.813l0.099,-0.029l-14.02,5l0,-7.958l0.801,-0.283c2.801,-0.931 6.093,-1.654 9.489,-2.03l0.221,-0.022c1.249,-0.133 2.695,-0.208 4.16,-0.208c5.007,0 9.809,0.883 14.254,2.505l-0.288,-0.093c6.16,2.003 6.8,4.906 5.252,6.907c-1.491,1.486 -3.294,2.662 -5.303,3.42l-0.104,0.035l-28.484,10.359l-0,-7.654l0.069,-0.078Zm-15.521,-54.146l-0,58.498l13.051,4.204l-0,-49.061c-0,-2.3 1.013,-3.836 2.646,-3.303c2.121,0.601 2.534,2.713 2.534,5.018l0,19.587c8.137,3.978 14.543,-0.008 14.543,-10.508c-0,-10.791 -3.754,-15.585 -14.796,-19.427c-4.762,-1.729 -10.778,-3.452 -16.933,-4.815l-1.037,-0.193l-0.008,0Z"
@@ -194,12 +226,21 @@ svg {
         @apply stroke-4px stroke-black/80 dark-stroke-white/80 fill-none;
     }
 
+    .ds-fill-red {
+        @apply fill-red-700;
+    }
+
+    .ds-stroke-dashed {
+        @apply stroke-2px stroke-black/50 dark-stroke-white/50 fill-none;
+        stroke-dasharray: 5;
+    }
+
     .ds-ps-icon {
         @apply fill-black/20 dark-fill-white/20;
     }
 
     .ds-stick {
-        @apply fill-white dark-fill-black;
+        @apply fill-white/80 dark-fill-black/80;
     }
 
     .ds-active {
