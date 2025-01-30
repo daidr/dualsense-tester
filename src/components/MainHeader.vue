19a96759bd56fdf391547d9ba69c51fc295f9220
daidr <daidr@daidr.me>
2025-01-30 21:18:41 +0800

    chore: update header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -13,10 +13,10 @@ import LangSwitcher from './LangSwitcher.vue'
       <div class="tools-wrapper">
         <LangSwitcher />
         <DarkModeSwitch />
-        <a href="https://producthunt.com/products/dualsense-tester" target="_blank" class="dou-icon-link">
-          <div class="i-mingcute-thumb-up-2-line" />
+        <a href="https://buymeacoffee.com/daidr" target="_blank" class="dou-icon-link" title="Buy me a coke">
+          <div class="i-mingcute-glass-cup-line" />
         </a>
-        <a href="https://github.com/daidr/dualsense-tester" target="_blank" class="dou-icon-link">
+        <a href="https://github.com/daidr/dualsense-tester" target="_blank" class="dou-icon-link" title="GitHub">
           <div class="i-mingcute-github-line" />
         </a>
       </div>
@@ -26,25 +26,43 @@ import LangSwitcher from './LangSwitcher.vue'
 
 <style scoped lang="scss">
 header {
-    @apply select-none;
-    @apply w-full px-2;
+  @apply select-none;
+  @apply w-full px-2;
 
-    .header {
-        @apply max-w-[var(--max-width)] w-full text-lg;
-        @apply flex justify-between items-center mx-auto my-3;
-        @apply dou-sc-container;
+  .header {
+    @apply max-w-[var(--max-width)] w-full text-lg;
+    @apply flex justify-between items-center mx-auto my-3;
+    @apply dou-sc-container;
+  }
+
+  .logo-wrapper {
+    @apply flex items-center gap-2 text-primary font-bold;
+
+    h1 {
+      @apply hidden sm:block;
+    }
+  }
+
+  .tools-wrapper {
+    @apply flex items-center gap-4;
+  }
+
+  .dou-icon-link {
+    @apply relative;
+
+    &::after {
+      content: attr(title);
+      @apply absolute left-1/2 -bottom-2 text-xs whitespace-nowrap break-keep;
+      @apply transform-gpu -translate-x-1/2 translate-y-full scale-10 opacity-0 transform-origin-tc;
+      @apply bg-primary px-2 py-1 rounded-full;
+      @apply 'transition-transform,opacity delay-100 duration-200';
     }
 
-    .logo-wrapper {
-        @apply flex items-center gap-2 text-primary font-bold;
-
-        h1 {
-            @apply hidden sm:block;
-        }
+    &:hover {
+      &::after {
+        @apply scale-100 opacity-100;
+      }
     }
-
-    .tools-wrapper {
-        @apply flex items-center gap-4;
-    }
+  }
 }
 </style>
