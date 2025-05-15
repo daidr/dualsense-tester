bdd4d7b2ee43df94be3102c6db99f1f6f9076eba
Xuezhou Dai <daidr@daidr.me>
2025-05-16 00:05:43 +0800

    fix: wrong PWA condition

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { crowdinDefine } from './config/crowdin'
 import { gitDefine } from './config/git'
 
-const isVercelProduction = true
+const isVercelProduction = process.env.VERCEL_ENV === 'production'
 
 // import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
